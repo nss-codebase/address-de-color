32b2f9879b24bb12b8a168e4b8e6a7db070ac794
Chyld Medford <chyld.medford@gmail.com>
2014-08-01 16:35:14 -0500

    really done

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -6,9 +6,9 @@ module.exports = function(name, cb){
   var url = 'mongodb://localhost/' + name;
   MongoClient.connect(url, function(err, db){
     global.mongodb = db;
-    if(cb){cb();}
 
     console.log('MongoDB Ready:', name);
+    if(cb){cb();}
   });
 };
 
